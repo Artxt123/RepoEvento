ab6ab81d22a40fd8d2f69207105edb5aab3a1484
Artur Oczkowski <alfaix@o2.pl>
2021-11-15 23:37:53 +0100

    2.3  Domena oraz encja

---
+++ src/Evento.Core/Domain/Entity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Evento.Core.Domain
+{
+    /// <summary>
+    /// Summary description for Class1
+    /// </summary>
+    public abstract class Entity
+    {
+        public Guid Id { get; private set; }
+
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
+
+
